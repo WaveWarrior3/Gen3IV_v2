7a1565069a2ff1c02e894472922bbde7f16c4e02
WaveWarrior <wavewarrior488@gmail.com>
2023-09-24 17:27:15 -0600

    Reformatted compile scripts

--- bin/compile.sh
+++ bin/compile.sh
@@ -5,7 +5,8 @@ echo Removing Gen3Predictor.jar
 rm ../Gen3Predictor.jar 
 
 echo Compiling Java code
-javac -cp /home/jacob/git/Gen3IV_v2/src -d /home/jacob/git/Gen3IV_v2/bin/ /home/jacob/git/Gen3IV_v2/src/gen3check/Main.java
+#javac -cp /home/jacob/git/Gen3IV_v2/src -d /home/jacob/git/Gen3IV_v2/bin/ /home/jacob/git/Gen3IV_v2/src/gen3check/Main.java
+javac -cp ../src -d ./ ../src/gen3check/Main.java
 
 echo Building into jar file
 jar cfe ../Gen3Predictor.jar gen3check.Main ./gen3check/* ./gen3check/gui/* ./gen3check/observers/* ./gen3check/pokemon/* ./gen3check/predictor/* ./gen3check/util/* ./rng/* ./database/* ./font/* ./image/*
